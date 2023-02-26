96e78b6d7fff200b91fcda7651f088877e5fcf93
Christian Schroeder de Witt <schroederdewitt@gmail.com>
2023-02-26 19:28:03 +0000

    minor

--- train_cifar_large.sh
+++ train_cifar_large.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /home/duser/venv/bin/activate
-python3 s4/train.py dataset=cifar layer=s4 train.epochs=1000 train.bsz=64 train.lr=5e-3 train.lr_schedule=true model.d_model=512 model.n_layers=6 model.dropout=0.0 train.weight_decay=0.05 model.prenorm=true model.embedding=true wandb.mode=online train.sample=308 train.checkpoint=1
+python3 s4/train.py dataset=cifar layer=s4 train.epochs=1000 train.bsz=64 train.lr=5e-3 train.lr_schedule=true model.d_model=512 model.n_layers=6 model.dropout=0.0 train.weight_decay=0.05 model.prenorm=true model.embedding=true wandb.mode=offline train.sample=308 train.checkpoint=1
