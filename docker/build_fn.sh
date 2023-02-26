4f8efd892c4ff1f132c6f6ac97c2f3374573e60c
Christian Schroeder de Witt <schroederdewitt@gmail.com>
2023-02-26 18:51:25 +0000

    added build scripts

---
+++ docker/build_fn.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'Building Dockerfile with image name annotated_s4'
+# docker build --no-cache --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)  -t annotated_s4 .
+docker build --build-arg USER_ID=0 --build-arg GROUP_ID=0  -t annotated_s4 .
