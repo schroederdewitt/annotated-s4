a20782dafa635ab49339a7e7a878e9208be61206
Christian Schroeder de Witt <schroederdewitt@gmail.com>
2023-02-26 18:52:23 +0000

    updated

--- docker/build_fn.sh
+++ docker/build_fn.sh
@@ -2,4 +2,4 @@
 
 echo 'Building Dockerfile with image name annotated_s4'
 # docker build --no-cache --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)  -t annotated_s4 .
-docker build --build-arg USER_ID=0 --build-arg GROUP_ID=0  -t annotated_s4 .
+docker build --build-arg USER_ID=1001 --build-arg GROUP_ID=1001  -t annotated_s4 .
